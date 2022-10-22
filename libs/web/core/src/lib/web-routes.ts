7d275a3f01a809939b1e8f71ea802516be532dd1
giangtm210592 <giangtm210592@gmail.com>
2022-10-22 18:29:26 +0700

    setup router

--- libs/web/core/src/lib/web-routes.ts
+++ libs/web/core/src/lib/web-routes.ts
@@ -1,3 +1,24 @@
 import { Route } from '@angular/router';
+import { WebLayoutComponent } from '@nx-ws/web/shared/ui/web-layout';
 
-export const webRoutes: Route[] = [];
+export const webRoutes: Route[] = [
+  {
+    path: '',
+    pathMatch: 'full',
+    loadChildren: async () => (await import('@nx-ws/web/feat/landing-page')).LandingPageModule
+  },
+  {
+    path: 'login',
+    loadChildren: async () => (await import('@nx-ws/web/feat/login-page')).LoginPageModule
+  },
+  {
+    path: '',
+    component: WebLayoutComponent,
+    children: [
+      {
+        path: 'home',
+        loadChildren: async () => (await import('@nx-ws/web/feat/home-page')).HomePageModule
+      }
+    ]
+  }
+];
